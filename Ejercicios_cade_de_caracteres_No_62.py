f56b8737a1386d06bbe5f9aa262284cb14e75b42
Andrescpyo <142014854+Andrescpyo@users.noreply.github.com>
2023-08-28 22:14:56 -0500

    terminado ejercicio 62

---
+++ Ejercicios_cade_de_caracteres_No_62.py
@@ -0,0 +1,10 @@
+#62. Escribir un programa que compare dos cadenas de caracteres y nos diga si son idénticos o no. 
+
+cadena1 = input("\nIntroduce la primera cadena de caracteres: ")
+cadena2 = input("Introduce la segunda cadena de caracteres: ")
+
+# Comparamos las dos cadenas de caracteres.
+if cadena1 == cadena2:
+    print("\nLas dos cadenas de caracteres son idénticas.")
+else:
+    print("\nLas dos cadenas de caracteres no son idénticas.")
